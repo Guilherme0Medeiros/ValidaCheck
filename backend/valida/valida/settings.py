e5f1ae3bd3062c89bede82b1fa7c670cbaef14a1
João Vitor <jvitormaced@gmail.com>
2025-11-15 11:02:59 -0300

    configura MEDIA_URL e MEDIA_ROOT para servir arquivos

--- backend/valida/valida/settings.py
+++ backend/valida/valida/settings.py
@@ -109,6 +109,12 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True, }
 
 
+STATIC_URL = "static/"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 SECURE_COOKIE = False
