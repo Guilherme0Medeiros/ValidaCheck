a7d28036aa702f059b559fe404ea245dfbde93c7
João Vitor <jvitormaced@gmail.com>
2025-11-15 11:02:19 -0300

    adiciona rota de media files para modo DEBUG

--- backend/valida/valida/urls.py
+++ backend/valida/valida/urls.py
@@ -6,6 +6,9 @@ from drf_spectacular.views import (
     SpectacularRedocView,
 )
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
@@ -16,3 +19,7 @@ urlpatterns = [
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
